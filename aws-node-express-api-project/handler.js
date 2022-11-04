4ebeb33366ac9c16ba9ccf4ef0ed954e28a89d2c
David Pradell <david.pradell@moralis.io>
2022-11-04 13:58:19 +0100

    Updated Express API handler

--- aws-node-express-api-project/handler.js
+++ aws-node-express-api-project/handler.js
@@ -5,12 +5,13 @@ const { EvmChain } = require("@moralisweb3/evm-utils")
 
 const app = express();
 
-const address = "0xEaB4694375055D1C5D84C35Ee543D5b656980073";
-const chain = EvmChain.MUMBAI;
+const MORALIS_API_KEY = "replace me";
+const address = "replace me";
+const chain = EvmChain.ETHEREUM;
 
 const startMoralis = async () => {
   await Moralis.start({
-    apiKey: process.env.MORALIS_API_KEY,
+    apiKey: MORALIS_API_KEY,
   });
 };
 
