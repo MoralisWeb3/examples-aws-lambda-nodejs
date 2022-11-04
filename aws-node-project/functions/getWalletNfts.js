7803c873ecdbf1b15f5ff94a337f5c3bff458cba
David Pradell <david.pradell@moralis.io>
2022-11-04 13:41:21 +0100

    Updated return values

--- aws-node-project/functions/getWalletNfts.js
+++ aws-node-project/functions/getWalletNfts.js
@@ -2,12 +2,13 @@
 const Moralis = require('moralis').default;
 const { EvmChain } = require("@moralisweb3/evm-utils")
 
-const address = "0xEaB4694375055D1C5D84C35Ee543D5b656980073";
+const MORALIS_API_KEY = "replace me";
+const address = "replace me";
 const chain = EvmChain.MUMBAI;
 
 const startMoralis = async () => {
   await Moralis.start({
-    apiKey: process.env.MORALIS_API_KEY,
+    apiKey: MORALIS_API_KEY,
   });
 };
 
@@ -20,5 +21,7 @@ module.exports.handler = async (event) => {
     chain,
   });
 
-  return nfts;
+  return {
+    result: nfts
+  }
 };
